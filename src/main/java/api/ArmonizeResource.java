37c533fe3c17cc83ea23b193961cf74d0ca5e85c
Cristian <delcerro.cristan@gmail.com>
2014-01-11 14:18:06 +0100

    Post realizad

--- src/main/java/api/ArmonizeResource.java
+++ src/main/java/api/ArmonizeResource.java
@@ -3,9 +3,13 @@ package api;
 
 import com.google.common.base.Optional;
 import com.yammer.metrics.annotation.Timed;
+import org.apache.pdfbox.io.IOUtils;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.File;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 
@@ -24,5 +28,15 @@ public class ArmonizeResource {
     public ArrayList<String> sayHello(@QueryParam("name") Optional<String> name) {
         msg.add(name.or("anonymous"));
         return msg;
+    }
+
+    @POST
+    public Response uploadFile(final InputStream stream) throws Exception {
+
+        byte[] data = IOUtils.toByteArray(stream);
+        File temporal = new File("/tmp/tempFile.pdf" );
+        org.apache.commons.io.FileUtils.writeByteArrayToFile(temporal, data);
+
+        return Response.status(200).build();
     }
 }
