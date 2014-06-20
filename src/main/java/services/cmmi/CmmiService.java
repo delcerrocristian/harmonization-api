46d16ae563b4802b8fe60fd8a923c28177f64d70
delcerrocristian <delcerro.cristian@gmail.com>
2014-06-20 20:49:02 +0200

    CmmiService creado

--- src/main/java/services/cmmi/CmmiService.java
+++ src/main/java/services/cmmi/CmmiService.java
@@ -1,7 +1,48 @@
 package services.cmmi;
 
+import persistence.firstfilter.cmmi.model.*;
+import persistence.firstfilter.cmmi.model.Process;
+
+import java.util.ArrayList;
+
 /**
- * Created by evan on 16/06/14.
+ * Created by Cristian del Cerro
  */
 public interface CmmiService {
+
+    int createStandard(Standard standard);
+
+    int createStandardWithName(String name);
+
+    Standard readStandard(int id);
+
+    void deleteStandard(int id);
+
+    ArrayList<Standard> readAllStandards();
+
+    int createProcess(persistence.firstfilter.cmmi.model.Process process);
+
+    Process readProcess(int id);
+
+    ArrayList<persistence.firstfilter.cmmi.model.Process> readAllProcessByStandard(int idStandard);
+
+    int createSpecificGoal(SpecificGoal specificGoal);
+
+    SpecificGoal readSpecificGoal(int id);
+
+    ArrayList<SpecificGoal> readAllSpecificGoalByProcess(int idProcess);
+
+    int createSpecificPractice(SpecificPractice specificPractice);
+
+    SpecificPractice readSpecificPractice(int id);
+
+    ArrayList<SpecificPractice> readAllSpecificPracticeBySpecificGoal(int idSpecificGoal);
+
+    int createWorkProduct(WorkProduct workProduct);
+
+    WorkProduct readWorkProduct(int id);
+
+    ArrayList<WorkProduct> readAllWorkProductBySpecificPractice(int idSpecificPractice);
+
 }
+
---
+++ src/main/java/services/cmmi/CmmiServiceImp.java
@@ -0,0 +1,113 @@
+package services.cmmi;
+
+import persistence.firstfilter.cmmi.dao.*;
+import persistence.firstfilter.cmmi.model.*;
+import persistence.firstfilter.cmmi.model.Process;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Cristian del Cerro
+ */
+public class CmmiServiceImp implements CmmiService {
+
+    private StandardDao standardDao;
+    private ProcessDao processDao;
+    private SpecificGoalDao specificGoalDao;
+    private SpecificPracticeDao specificPracticeDao;
+    private WorkProductDao workProductDao;
+
+    public CmmiServiceImp(StandardDao standardDao, SpecificGoalDao specificGoalDao, ProcessDao processDao,
+                          SpecificPracticeDao specificPracticeDao, WorkProductDao workProductDao) {
+        this.standardDao = standardDao;
+        this.specificGoalDao = specificGoalDao;
+        this.processDao = processDao;
+        this.specificPracticeDao = specificPracticeDao;
+        this.workProductDao = workProductDao;
+    }
+
+    @Override
+    public int createStandard(Standard standard) {
+        return standardDao.create(standard);
+    }
+
+    @Override
+    public int createStandardWithName(String name) {
+        return standardDao.create(name);
+    }
+
+    @Override
+    public Standard readStandard(int id) {
+        return standardDao.read(id);
+    }
+
+    @Override
+    public void deleteStandard(int id) {
+        standardDao.delete(id);
+    }
+
+    @Override
+    public ArrayList<Standard> readAllStandards() {
+        return standardDao.readAll();
+    }
+
+    @Override
+    public int createProcess(persistence.firstfilter.cmmi.model.Process process) {
+        return processDao.create(process);
+    }
+
+    @Override
+    public Process readProcess(int id) {
+        return processDao.read(id);
+    }
+
+    @Override
+    public ArrayList<Process> readAllProcessByStandard(int idStandard) {
+        return processDao.readAllByStandard(idStandard);
+    }
+
+    @Override
+    public int createSpecificGoal(SpecificGoal specificGoal) {
+        return specificGoalDao.create(specificGoal);
+    }
+
+    @Override
+    public SpecificGoal readSpecificGoal(int id) {
+        return specificGoalDao.read(id);
+    }
+
+    @Override
+    public ArrayList<SpecificGoal> readAllSpecificGoalByProcess(int idProcess) {
+        return specificGoalDao.readAllByProcess(idProcess);
+    }
+
+    @Override
+    public int createSpecificPractice(SpecificPractice specificPractice) {
+        return specificPracticeDao.create(specificPractice);
+    }
+
+    @Override
+    public SpecificPractice readSpecificPractice(int id) {
+        return specificPracticeDao.read(id);
+    }
+
+    @Override
+    public ArrayList<SpecificPractice> readAllSpecificPracticeBySpecificGoal(int idSpecificGoal) {
+        return specificPracticeDao.readAllBySpecificGoal(idSpecificGoal);
+    }
+
+    @Override
+    public int createWorkProduct(WorkProduct workProduct) {
+        return workProductDao.create(workProduct);
+    }
+
+    @Override
+    public WorkProduct readWorkProduct(int id) {
+        return workProductDao.read(id);
+    }
+
+    @Override
+    public ArrayList<WorkProduct> readAllWorkProductBySpecificPractice(int idSpecificPractice) {
+        return workProductDao.readAllBySpecificPractice(idSpecificPractice);
+    }
+}
