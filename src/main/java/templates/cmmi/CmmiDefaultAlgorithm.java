57d8b3a4b770f115e203ae9d2cc41520f277e2ee
delcerrocristian <delcerro.cristian@gmail.com>
2014-06-23 18:44:55 +0200

    plantilla acabada pero con errores en los indices/patrones

--- src/main/java/templates/cmmi/CmmiDefaultAlgorithm.java
+++ src/main/java/templates/cmmi/CmmiDefaultAlgorithm.java
@@ -1,5 +1,7 @@
 package templates.cmmi;
 
+import persistence.firstfilter.cmmi.model.*;
+import persistence.firstfilter.cmmi.model.Process;
 import services.cmmi.CmmiService;
 
 import java.util.ArrayList;
@@ -13,6 +15,7 @@ public class CmmiDefaultAlgorithm implements CmmiAlgorithm{
 
     CmmiService cmmiService;
     ArrayList<String> processArea;
+    int idStandard;
 
     public CmmiDefaultAlgorithm(CmmiService cmmiService) {
         this.cmmiService = cmmiService;
@@ -21,7 +24,9 @@ public class CmmiDefaultAlgorithm implements CmmiAlgorithm{
 
     @Override
     public void start(ArrayList<String> listOfText, int idStandard) {
+        this.idStandard = idStandard;
         int currentIndex = findProcessArea(listOfText);
+        searchInformation(listOfText, currentIndex);
 
     }
 
@@ -54,10 +59,132 @@ public class CmmiDefaultAlgorithm implements CmmiAlgorithm{
         }
     }
 
-    private int findAndFillProcess(ArrayList<String> listOfText, String process, String endProcess, int index) {
+    private void searchInformation(ArrayList<String> listOfText, int index) {
+        for(String currentProcessArea : processArea){
+            findAndFillProcess(listOfText,currentProcessArea, index);
+        }
+
+    }
+
+    private int findAndFillProcess(ArrayList<String> listOfText, String process, int index) {
+        int currentIndex = 0;
+        persistence.firstfilter.cmmi.model.Process currentProcess = new Process();
+        currentProcess.setStandard(idStandard);
+
         for(int i = index; i< listOfText.size(); i++) {
+            if(searchThisPattern(process, listOfText.get(i))){
+                currentProcess.setName(process);
+                i++;
+                String[] sentenceWithAreaAndMaturityLevel = listOfText.get(i).split("Area");
+                currentProcess.setAreaCategory(sentenceWithAreaAndMaturityLevel[0].substring(2,
+                        sentenceWithAreaAndMaturityLevel[0].length()-1));
+                currentProcess.setMaturityLevel(sentenceWithAreaAndMaturityLevel[1].substring(
+                        sentenceWithAreaAndMaturityLevel[1].length()-2,
+                        sentenceWithAreaAndMaturityLevel[1].length()-1));
+
+                i = i + 2;
+                String purposeStatement = "";
+                while(!searchEndPurposeStatement(listOfText.get(i))){
+                    purposeStatement = purposeStatement + listOfText.get(i).toString() +" ";
+                    i++;
+                }
+                currentProcess.setPurposeStatement(purposeStatement);
+                int idProcess = cmmiService.createProcess(currentProcess);
+                currentIndex = findSpecificGoals(process, listOfText, i, idProcess);
+                break;
+            }
+
 
         }
-        return 0;
+        return currentIndex;
+    }
+
+    private int findSpecificGoals(String process, ArrayList<String>  listOfText, int index, int idProcess) {
+        ArrayList<String> listGoals = new ArrayList<String>();
+        boolean open = false;
+        int currentIndex = index;
+
+        /*while(!searchThisPattern(process, listOfText.get(currentIndex))) {
+            currentIndex++;
+        }*/
+
+        for(int i = currentIndex; i<listOfText.size(); i++) {
+            if(!open && searchStartListGoals(listOfText.get(i))) {
+                open = true;
+            }
+            else if(open && searchStartSpecificsGoal(listOfText.get(i))) {
+                currentIndex = i;
+                break;
+            }
+            else if(open) {
+                listGoals.add(listOfText.get(i));
+            }
+        }
+
+        int idSpecificGoal=-1;
+
+        for(String elementGoal : listGoals){
+            if(isGoal(elementGoal)){
+                SpecificGoal specificGoal = new SpecificGoal();
+                specificGoal.setProcess(idProcess);
+                specificGoal.setTitle(elementGoal.substring(5));
+                String description= "";
+                for (int i = currentIndex; i < listOfText.size(); i++) {
+                    if(searchThisPattern(elementGoal, listOfText.get(i))){
+                        i++;
+                        for(int j=i ;j<listOfText.size();j++) {
+                            description = description + listOfText.get(j) + " ";
+                            if (searchFinalDot(listOfText.get(j))) {
+                                currentIndex = j;
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+                specificGoal.setDescription(description);
+                idSpecificGoal = cmmiService.createSpecificGoal(specificGoal);
+            }
+            else if(isPractice(elementGoal)){
+                int idSpecificPractice = -1;
+                SpecificPractice specificPractice = new SpecificPractice();
+                specificPractice.setSpecificGoal(idSpecificGoal);
+                specificPractice.setTitle(elementGoal.substring(7));
+                String description = "";
+                for(int i = currentIndex; i<listOfText.size(); i++) {
+                    if(searchThisPattern(elementGoal, listOfText.get(i))) {
+                        while (!searchFinalDot(listOfText.get(i))){
+                            i++;
+                        }
+                        i++;
+                        for(int j=i;j<listOfText.size();j++) {
+                            description = description + listOfText.get(j) + " ";
+                            if (searchFinalDot(listOfText.get(j))) {
+                                currentIndex = j;
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+                specificPractice.setDescription(description);
+                idSpecificPractice = cmmiService.createSpecificPractice(specificPractice);
+
+                while(!searchStartWorkProduct(listOfText.get((currentIndex)))){
+                    currentIndex++;
+                }
+                currentIndex++;
+                while(!searchEndWorkProduct(listOfText.get(currentIndex))) {
+                    WorkProduct workProduct = new WorkProduct();
+                    workProduct.setSpecificPractice(idSpecificPractice);
+                    workProduct.setDescription(listOfText.get(currentIndex).substring(3));
+                    cmmiService.createWorkProduct(workProduct);
+                    currentIndex++;
+                }
+
+            }
+
+        }
+        return currentIndex;
     }
 }
--- src/main/java/templates/cmmi/SearchPatterns.java
+++ src/main/java/templates/cmmi/SearchPatterns.java
@@ -15,7 +15,42 @@ public class SearchPatterns {
         return Pattern.compile(".*Purpose Statements.*").matcher(sentence).matches();
     }
 
-    public static boolean searchProcessArea(String sentence){
-        return Pattern.compile(".*Purpose Statements.*").matcher(sentence).matches();
+    public static boolean searchThisPattern(String pattern, String sentence){
+        String currentSentence = ".*"+pattern+".*";
+        return Pattern.compile(currentSentence).matcher(sentence).matches();
     }
+
+    public static boolean searchEndPurposeStatement(String sentence){
+        return Pattern.compile(".*Introductory Notes.*").matcher(sentence).matches();
+    }
+
+    public static boolean searchStartListGoals(String sentence) {
+        return Pattern.compile(".*Specific Goal and Practice Summary .*").matcher(sentence).matches();
+    }
+
+    public static boolean searchStartSpecificsGoal(String sentence) {
+        return Pattern.compile(".*Specific Practices by Goal.*").matcher(sentence).matches();
+    }
+
+    public static boolean isGoal(String sentence) {
+        return Pattern.compile("^SG.*").matcher(sentence).matches();
+    }
+
+    public static boolean isPractice(String sentence) {
+        return Pattern.compile("^SP.*").matcher(sentence).matches();
+    }
+
+    public static boolean searchFinalDot(String sentence) {
+        return Pattern.compile(".*[.] $").matcher(sentence).matches() ||
+                Pattern.compile(".*[.]$").matcher(sentence).matches();
+    }
+
+    public static boolean searchEndWorkProduct(String sentence) {
+        return Pattern.compile(".*Subpractices.*").matcher(sentence).matches();
+    }
+    public static boolean searchStartWorkProduct(String sentence) {
+        return Pattern.compile(".*Example Work Products.*").matcher(sentence).matches();
+    }
+
+
 }
